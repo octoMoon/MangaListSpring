7e78a41ae9243394e11f9e16da4dc843a7790ea5
redcat <xvaldmir88x@gmail.com>
2022-10-24 23:32:57 +0700

    add add-fun

--- src/main/java/com/octo/mangaList/controller/MangaController.java
+++ src/main/java/com/octo/mangaList/controller/MangaController.java
@@ -5,52 +5,42 @@
 package com.octo.mangaList.controller;
 
 import com.octo.mangaList.entity.MangaEntity;
-import com.octo.mangaList.exception.TitleAlreadyExistException;
-import com.octo.mangaList.exception.TitleNotFoundByIdException;
-import com.octo.mangaList.service.MangaService;
+import com.octo.mangaList.repository.MangaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author vladmir
  */
-@RestController
-@RequestMapping("/manga")
+@Controller
 public class MangaController {
 
     @Autowired
-    MangaService mangaService;
+    private MangaRepository mangaRepository;
 
-    @PostMapping
-    public ResponseEntity addMangaTitle(@RequestBody MangaEntity manga) {
-        try {
-            mangaService.addMangaTitle(manga);
-            return ResponseEntity.ok("Add new Title");
-        } catch (TitleAlreadyExistException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Error");
-        }
+    @GetMapping("/main")
+    public String main(Model model) {
+        Iterable<MangaEntity> mangas = mangaRepository.findAll();
+        model.addAttribute("mangas", mangas);
+        return "main";
     }
 
-    @GetMapping
-    public ResponseEntity getMangaTitleById(@RequestParam Long id) {
-        try {
-            return ResponseEntity.ok(mangaService.getTitleFindById(id));
-        } 
-        catch (TitleNotFoundByIdException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-        catch (Exception e) {
-            return ResponseEntity.badRequest().body("Error");
-        }
-    } 
+    @GetMapping("/addtitle")
+    public String addtitle(Model model) {
+        return "addtitle";
+    }
+
+    @PostMapping("/addtitle")
+    public String addtitlepost(@RequestParam String title, Model model) {
+        MangaEntity manga = new MangaEntity();
+        manga.setTitle(title);
+        mangaRepository.save(manga);
+        return "redirect:/main";
+    }
 
 }
