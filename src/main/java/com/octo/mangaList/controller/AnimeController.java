8df34cdf4d61019ae9576ae451c614e4806d75ea
redcat <xvaldmir88x@gmail.com>
2022-11-06 14:57:53 +0700

    add button for checkbox

--- src/main/java/com/octo/mangaList/controller/AnimeController.java
+++ src/main/java/com/octo/mangaList/controller/AnimeController.java
@@ -4,11 +4,14 @@
  */
 package com.octo.mangaList.controller;
 
+import com.octo.mangaList.entity.AnimeEntity;
 import com.octo.mangaList.service.AnimeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,7 +34,7 @@ public class AnimeController {
 
     @GetMapping("/anime/{id}")
     public String details(@PathVariable(value = "id") Long id, Model model) {
-        animeService.details(id, model);
+        animeService.details(id, model);      
         return "animedetails";
     }
     
@@ -39,6 +42,12 @@ public class AnimeController {
     public String addTitlePost(@RequestParam String title, @RequestParam int episodes, Model model) {
         animeService.addTitle(title,episodes, model);
         return "redirect:/anime";
+    }
+    
+     @PostMapping("/anime/{id}")
+    public String watching(@PathVariable(value = "id") Long id, @RequestParam int episodes, Model model) {
+        animeService.watching(id, episodes, model);
+        return "redirect:/anime/{id}";
     }
 
 }
--- src/main/java/com/octo/mangaList/service/AnimeService.java
+++ src/main/java/com/octo/mangaList/service/AnimeService.java
@@ -27,7 +27,7 @@ public class AnimeService {
         model.addAttribute("animes", animes);
         return "main";
     }
-    
+
     public String addTitle(String title, int episodes, Model model) {
         AnimeEntity anime = new AnimeEntity();
         anime.setTitle(title);
@@ -36,15 +36,58 @@ public class AnimeService {
         return "redirect:/anime";
     }
 
+    public String watching(@PathVariable(value = "id")Long id,@RequestParam int episodes, Model model) {
+        AnimeEntity anime = animeRepository.findById(id).orElseThrow();
+        model.addAttribute("id", anime.getId());
+        anime.setWatchedEpisodes(anime.getWatchedEpisodes() + episodes);
+        animeRepository.save(anime);
+        return "animedetails";
+    }
+
     public String details(@PathVariable(value = "id") Long id, Model model) {
         AnimeEntity anime = animeRepository.findById(id).orElseThrow();
-        String[] animes = new String[anime.getAllEpisodes()];
+        Episode[] animes = new Episode[anime.getAllEpisodes()];
         for (int i = 0; i < anime.getAllEpisodes(); i++) {
-            animes[i] = "Эпизод " + ++i;
-            --i;
+            if (i < anime.getWatchedEpisodes()) {
+                animes[i] = new Episode(++i, true, anime.getId());
+                --i;
+            } else {
+                animes[i] = new Episode(++i, false, anime.getId());
+                --i;
+            }
         }
         model.addAttribute("animes", animes);
         return "animedetails";
+    }
+
+    class Episode {
+        
+        Long id;
+        int number;
+        boolean watched;
+
+        public Episode(int number, boolean watched, Long id) {
+            this.id = id;
+            this.number = number;
+            this.watched = watched;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public void setNumber(int number) {
+            this.number = number;
+        }
+
+        public boolean isWatched() {
+            return watched;
+        }
+
+        public void setWatched(boolean watched) {
+            this.watched = watched;
+        }
+
     }
 
 }
